e08203f619b342f1dd6b548dd0955274d3458057
Dave Mateer home <davemateer@gmail.com>
2014-04-04 07:18:51 +0100

    Building up Story and StoryCreator.  Added more tests for invalid.

---
+++ Specs/CRUD/InvalidStoryReceived.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Specs.CRUD {
+    [Trait("An Invalid Story is Received", "")]
+    public class InValidStoryReceived {
+        [Fact(DisplayName = "A Story is not added to the system")]
+        public void StoryNotAddedToSystem() {
+            throw new NotImplementedException();
+        }
+
+        [Fact(DisplayName = "A message is shown to the administrator explaining why")]
+        public void MessageShown() {
+            throw new NotImplementedException();
+        }
+    }
+
+}
