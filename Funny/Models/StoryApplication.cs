222f1a107962219b43875ae290fe37fdecadbd7b
Dave Mateer home <davemateer@gmail.com>
2014-04-08 07:26:59 +0100

    Refactoring and moving around status's to make workflow logical.  A storyApplication has an IsValid and a Staus of Pneding, Accepted or Denied.  And a message.

--- Funny/Models/StoryApplication.cs
+++ Funny/Models/StoryApplication.cs
@@ -1,10 +1,31 @@
 ﻿
+using System;
 namespace Funny.Models {
-    public class StoryApplication {
-        public bool HasBeenValidated { get; set; }
 
-        public StoryApplication() {
-            this.HasBeenValidated = false;
+    public enum StoryApplicationStatus {
+        Pending,
+        Accepted,
+        Denied
+    }
+
+    public class StoryApplication {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public StoryType StoryType { get; set; }
+        public bool IsValid { get; set; }
+        public StoryApplicationStatus Status { get; set; }
+        public string Message { get; set; }
+
+        public StoryApplication(string title, string content, StoryType storyType) {
+            this.Title = title;
+            this.Content = content;
+            this.StoryType = storyType;
+            this.IsValid = false;
+            this.Status = StoryApplicationStatus.Pending;
+
+            if (String.IsNullOrWhiteSpace(this.Title)
+               || String.IsNullOrWhiteSpace(this.Content))
+                throw new InvalidOperationException("Can't have an empty Title or Content");
         }
     }
 }
--- Funny/Services/StoryCreator.cs
+++ Funny/Services/StoryCreator.cs
@@ -1,36 +1,44 @@
 ﻿using Funny.Models;
 
 namespace Funny.Services {
-public class StoryCreatorResult {
-    public Story NewStory { get; set; }
-    public string Message { get; set; }
-    public bool Success { get; set; }
-    public StoryApplication StoryApplication { get; set; }
+    public class StoryCreatorResult {
+        public Story NewStory { get; set; }
+        public StoryApplication StoryApplication { get; set; }
 
-    public StoryCreatorResult() {
-        this.Success = false;
-    }
-}
-
-public class StoryCreator {
-    public StoryApplication ValidateApplication(StoryApplication app) {
-        // do some stuff
-        app.HasBeenValidated = true;
-        return app;
+        public StoryCreatorResult() {
+        }
     }
 
-    public StoryCreatorResult CreateNewStory(StoryApplication app) {
-        var result = new StoryCreatorResult();
+    public class StoryCreator {
+        public StoryApplication ValidateApplication(StoryApplication app) {
+            // do some stuff
 
-        // validation
-        result.StoryApplication = ValidateApplication(app);
+            if (app.Title.Length < 4) {
+                app.IsValid = false;
+                app.Status = StoryApplicationStatus.Denied;
+                app.Message = "Invalid - Title needs to be 4 or more characters";
+            } else {
+                app.IsValid = true;
+            }
 
-        // Successful creation of story!
-        result.NewStory = new Story();
-        result.Message = "Successfully created a new story!";
-        result.StoryApplication = app;
+            return app;
+        }
 
-        return result;
+        public StoryCreatorResult CreateNewStory(StoryApplication app) {
+            var result = new StoryCreatorResult();
+
+            // validation
+            result.StoryApplication = ValidateApplication(app);
+
+            if (result.StoryApplication.IsValid) {
+                // Successful creation of story!
+                result.NewStory = new Story();
+                result.StoryApplication.Message = "Successfully created a new story!";
+                result.StoryApplication.Status = StoryApplicationStatus.Accepted;
+                //result.StoryApplication = app;
+            }
+            
+            return result;
+        }
     }
-}
 }
