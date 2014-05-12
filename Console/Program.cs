fd80b638f5c5432d9f7582276dbaa4ce633c1187
Dave Mateer home <davemateer@gmail.com>
2014-05-12 07:37:51 +0100

    Adding in console importer project

---
+++ Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Core.DB;
+
+namespace ConsoleImporter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello world");
+            using (var session = new Session()){
+                // Read data
+                var jokes = session.Stories;
+                foreach (var joke in jokes){
+                    Console.WriteLine(joke.Title);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
